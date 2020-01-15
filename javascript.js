9d58db35417dc79d9635d9772e9775df32397a4a
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-01-14 22:20:58 -0800

    started js for calculator

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function getHistory()
+{
+  return document.getElementById("history-value").innerText;
+}
+function printHistory()
+{
+   document.getElementById("history-value").innerText=num;
+}
+function getOutput()
+{
+    return document.getElementsById("output-value").innetText;
+}
+function printOutput()
+{
+    document.getElementById("output-value").innetText=num;
+}
